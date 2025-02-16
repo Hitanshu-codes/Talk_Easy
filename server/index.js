adf247de75afaf8e196f1cb78fb06941d7e3d5c9
Hitmans-codes <154245536+Hitmans-codes@users.noreply.github.com>
2025-02-17 02:42:44 +0530

    functionality ready

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const AssemblyAi = require('assemblyai')
 const fs = require('fs');
 const { translateGoogleFree } = require('./translate.cjs');
 // const { OpenAI } = require('openai');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+
+
 require('dotenv').config();
+const gemini = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const client = new AssemblyAi.AssemblyAI({
     apiKey: process.env.ASSEMBLY_API_KEY
 })
@@ -33,12 +37,31 @@ app.post('/transcribe', upload.single('audio'), async (req, res) => {
         console.error(error);
     }
 });
+app.post('/suggest', async (req, res) => {
+    try {
+        const { text } = req.body;
+        const model = gemini.getGenerativeModel({ model: 'gemini-2.0-flash' });
+        const prompt = `Understand the conversation from the text and suggest 3 answers that should be given for the text.Return the response in valid JSON format like this:
+            {"suggestions": ["response 1","response 2","response 3"]}
+        Do not wrap the response in markdown (\`\`\`json ... \`\`\`). Return only the JSON object. Also do not include any string formatting in responses.
+    Strictly no extra text: ${text}`;
+
+        const result = await model.generateContent(prompt);
+        const jsonResponse = JSON.parse(result.response.text());
+        res.json({ suggestions: jsonResponse });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 app.post('/translate', async (req, res) => {
     try {
         console.log(req.body.text);
         console.log(req.body.targetLang);
         console.log("Translating...");
         const translatedText = await translateGoogleFree(req.body.text, req.body.targetLang);
+        console.log("Translated!");
+        console.log(translatedText);
 
         res.json({ translatedText });
 
--- server/translate.cjs
+++ server/translate.cjs
@@ -1,10 +1,25 @@
 const fetch = require("node-fetch");
+const extractTranslations = (data) => {
+    let Translations = [];
+
+    // Check if the response structure is valid
+    if (Array.isArray(data) && Array.isArray(data[0])) {
+        for (let item of data[0]) {
+            if (Array.isArray(item) && item.length > 0) {
+                Translations.push(item[0]);
+            }
+        }
+    }
+
+    return Translations;
+};
+
 
 exports.translateGoogleFree = async function translateGoogleFree(text, targetLang) {
     const response = await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}`);
     const data = await response.json();
-    console.log(data[0][0][0]);
-    const translatedText = data[0][0][0];
+
+    const translatedText = extractTranslations(data);
     return translatedText;
 }
 
